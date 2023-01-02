255ffb1ab9115939d6c69f921b24fb6431affdf8
IceCold-302 <76656400+IceCold-302@users.noreply.github.com>
2023-01-02 13:58:27 +0100

    removed tests with same result

--- tests.js
+++ tests.js
@@ -1,5 +1,4 @@
 const resolveVariables = require("./main");
-
 const map1 = {
   "key1": "foo${key2}bar",
   "key2": "${key3}baz",
@@ -10,47 +9,36 @@ try {
   console.log(resolveVariables(map1));
 } catch (e) {
   console.log(e.message);
-}
-// Output: "Circular reference detected: key1"
-
+};
 const map2 = {
   "key1": 123,
   "key2": "${key3}baz",
-  "key3": "${key4}",
-  "key4": "${key1}"
+  "key3": "${key4}k",
+  "key4": "${key1}lm"
 };
 try {
   console.log(resolveVariables(map2));
 } catch (e) {
   console.log(e.message);
-}
-// Output: "Invalid value for key: key1"
-
+};
 const map3 = {
   "key1": "foo${key2}bar",
   "key2": "${key3}baz",
   "key3": "${key4}",
   "key4": "qux"
 };
-console.log(resolveVariables(map3));
-
-const map4 = {
-  "key1": "foo${key2}bar",
-  "key2": "${key3}baz$",
-  "key3": "${key4}",
-  "key4": "qux"
+try {
+    console.log(resolveVariables(map3));
+  } catch (e) {
+    console.log(e.message);
 };
-console.log(resolveVariables(map4));
-
-const map5 = {
+const map4 = {
   "key1": "foo${key2}bar",
   "key2": "${key3}baz",
   "key3": "${key4}"
 };
-console.log(resolveVariables(map5));
-// Key not found 
-const map6 = {
-  "key1": "foo${key2}bar",
-  "key2": "${key3}baz",
-  "key3": "${key4}", }
-console.log(resolveVariables(map6))
+try {
+  console.log(resolveVariables(map4));
+} catch (e) {
+  console.log(e.message);
+};
